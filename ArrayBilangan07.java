22e3103a7c86009e287848bca6b85cbdb65ee154
SaSaaaaaaaaa <shrxnz2332@gmail.com>
2024-11-15 14:17:20 +0700

    Percobaan 1

--- ArrayBilangan07.java
+++ ArrayBilangan07.java
@@ -2,7 +2,7 @@ public class ArrayBilangan07{
     public static void main(String[] args) {
         int bil[]= {5, 13, -7, 17};
 
-        for (int i = 0; i < bil.length; i++){
+        for (int i = 0; i < 4; i++){
             System.out.println(bil[i]);
         }
     }
